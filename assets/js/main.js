d7a183560e746b84386f67fbd64220f9b4923ebf
MahshadOliaie <mahshadoliaie@gmail.com>
2024-01-14 14:45:07 +0330

    add some funcions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
 
 let newBtn = document.querySelector(".toolbar__add");
 let root = document.querySelector(".notesContainer");
+let nameInput = document.querySelector(".addBox__name")
+let addBtn = document.querySelector(".addBox__btns__confirm");
+let cancelBtn = document.querySelector(".addBox__btns__cancel");
+let box = document.querySelector(".addBox")
 
 
+
+function openBox(){
+    box.classList.remove("dnone")
+}
+
+function closeBox(){
+    box.classList.add("dnone")
+}
 
 
 function addNote() {
+    let name = nameInput.value ; 
+    DATA.push({"name":name, "date":"26 Apr 2023"})
+
+    renderNotes(DATA)
+    nameInput.value = ""
+    closeBox();
 }
 
 
